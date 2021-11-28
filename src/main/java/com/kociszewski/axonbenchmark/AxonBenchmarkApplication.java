25b8427b29a51fef3cf406b83603042a37e45298
mkociszewski <mkociszewski@consdata.pl>
2021-11-28 15:05:24 +0100

    Initial commit

---
+++ src/main/java/com/kociszewski/axonbenchmark/AxonBenchmarkApplication.java
@@ -0,0 +1,13 @@
+package com.kociszewski.axonbenchmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonBenchmarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxonBenchmarkApplication.class, args);
+    }
+
+}
